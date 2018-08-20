1854d0e10b6dc5a41432d0474c2255de24ba631d
dynamixXs <dynamixXs>
2018-08-19 21:14:10 -0400

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,14 +14,12 @@ app.use(express.static(publicPath));
 io.on("connection", (socket) => {
     console.log("new user connected");
 
-    socket.emit("newMessage", {
-        from: "Jimmy",
-        text: "See you soon",
-        createdAt: 123456
-    })
-
     socket.on("createMessage", (message) => {
-        console.log("createMessage", message);
+        io.emit("newMessage", {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on("disconnect", () => {
